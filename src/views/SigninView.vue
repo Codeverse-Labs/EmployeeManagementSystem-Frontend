302b72d06d1265598f8ba6ba6de126574083621b
thenuka99 <janith.h@creativesoftware.com>
2023-03-14 17:08:17 +0530

    update signin and signup pages

--- src/views/SigninView.vue
+++ src/views/SigninView.vue
@@ -4,8 +4,8 @@
     class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 fixed inset-0 z-10 overflow-y-auto bg-blue-50">
     <div class="w-full max-w-md space-y-8 bg-white py-16 px-12 rounded-lg drop-shadow-2xl">
       <div>
-        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-          Sign in
+        <h2 class="grid justify-items-center">
+          <img src="../assets/hasthiya-logo.png" alt="Logo" class="h-28 w-28 flex justify-center m-0">
         </h2>
       </div>
       <form class="mt-8 space-y-6" @submit.prevent="login" method="POST">
