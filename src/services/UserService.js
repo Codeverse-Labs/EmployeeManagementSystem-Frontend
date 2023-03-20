c67321e3f717bd754995a7fe4dd31f048d7fe95c
thenuka99 <janith.h@creativesoftware.com>
2023-03-21 00:21:07 +0530

    update user service

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -3,20 +3,46 @@ import  axios  from "axios";
 export class UserService{
     static serverURL = 'http://localhost:5000';
 
-    static getAllUsers(){
-        let dataURL = `${this.serverURL}/users`;
-        return axios.get(dataURL);
+    static getAllUsers(page){
+        let dataURL = `${this.serverURL}/users?page=${page}`;
+        return axios.get(dataURL,{headers: {
+            'Content-Type': 'application/json'
+        }});
+    }
+    static getAllUsersByTechnology(id,page){
+        let dataURL = `${this.serverURL}/users/technology/${id}?page=${page}`;
+        return axios.get(dataURL,{headers: {
+            'Content-Type': 'application/json'
+        }});
+    }
+    static getAllUsersByDesignation(id,page){
+        let dataURL = `${this.serverURL}/users/designation/${id}?page=${page}`;
+        return axios.get(dataURL,{headers: {
+            'Content-Type': 'application/json'
+        }});
+    }
+    static getAllUsersByName(searchTerm,page){
+        let dataURL = `${this.serverURL}/users/searchTerm/${searchTerm}?page=${page}`;
+        return axios.get(dataURL,{headers: {
+            'Content-Type': 'application/json'
+        }});
     }
     static getUser(userId){
         let dataURL = `${this.serverURL}/users/${userId}`;
-        return axios.get(dataURL);
+        return axios.get(dataURL,{headers: {
+            'Content-Type': 'application/json'
+        }});
     }
     static updateUser(user,userId){
         let dataURL = `${this.serverURL}/users/${userId}`;
-        return axios.put(dataURL,user);
+        return axios.put(dataURL,user,{headers: {
+            'Content-Type': 'application/json'
+        }});
     }
     static deleteUser(userId){
         let dataURL = `${this.serverURL}/users/${userId}`;
-        return axios.delete(dataURL);
+        return axios.delete(dataURL,{headers: {
+            'Content-Type': 'application/json'
+        }});
     }
 }
