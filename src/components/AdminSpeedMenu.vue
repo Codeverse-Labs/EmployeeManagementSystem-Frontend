42c8b1e227c20cab0b9a0e21ced455f1852cb1ea
thenuka99 <janith.h@creativesoftware.com>
2023-03-22 19:33:49 +0530

    update admin menu

--- src/components/AdminSpeedMenu.vue
+++ src/components/AdminSpeedMenu.vue
@@ -35,7 +35,7 @@
                 </button>
             </router-link>
 
-            <router-link to="/">
+            <router-link to="/assets">
                 <button type="button"
                     class="w-16 h-16 text-gray-500 bg-white rounded-lg border border-gray-200 hover:text-gray-900 dark:border-gray-600 shadow-sm dark:hover:text-white dark:text-gray-400 hover:bg-gray-50 dark:bg-gray-700 dark:hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 focus:outline-none dark:focus:ring-gray-400">
                     <i class=" fa fa-th-large"></i>
