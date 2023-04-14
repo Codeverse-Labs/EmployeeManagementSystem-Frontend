7beaa3a03baf75d914a94c8c21a5bcf01b1bfee3
thenuka99 <janith.h@creativesoftware.com>
2023-04-14 23:15:43 +0530

    update profile view

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,65 +1,6 @@
 <template>
   <!-- profile -->
-  <div class="p-4 bg-slate-200 md:p-16">
-    <div class="p-8 bg-white shadow-xl mt-24 rounded-lg">
-      <!-- user img -->
-      <div
-        class="relative w-40 h-40 mx-auto rounded-full shadow-2xl  top-0 -mt-24 flex items-center justify-center bg-white">
-        <img :src="userState.user.imageurl" alt="" class=" rounded-full ">
-      </div>
-
-      <div class="mt-20 border-b pb-12">
-        <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
-
-          <div class=" text-left">
-            <h1 class=" text-xs font-medium text-gray-400">Full Name</h1>
-            <h1 class="text-base font-medium text-gray-700" v-if="userState.user.name">{{ userState.user.name }} </h1>
-            <h1 class="text-base font-medium text-gray-700" v-else>-</h1>
-          </div>
-
-          <div class=" text-left">
-            <h1 class=" text-xs font-medium text-gray-400">Email</h1>
-            <h1 class="text-base font-medium text-gray-700" v-if="userState.user.email">{{ userState.user.email }} </h1>
-            <h1 class="text-base font-medium text-gray-700" v-else>-</h1>
-          </div>
-
-          <div class=" text-left">
-            <h1 class=" text-xs font-medium text-gray-400">Designation</h1>
-            <h1 class="text-base font-medium text-gray-700" v-if="userState.user.designation.name">{{
-              userState.user.designation.name }} </h1>
-            <h1 class="text-base font-medium text-gray-700" v-else>-</h1>
-          </div>
-
-          <div class=" text-left">
-            <h1 class=" text-xs font-medium text-gray-400">Birthday</h1>
-            <h1 class="text-base font-medium text-gray-700" v-if="userState.user.dob">{{ formatDate(userState.user.dob) }}
-            </h1>
-            <h1 class="text-base font-medium text-gray-700" v-else>-</h1>
-          </div>
-
-          <div class=" text-left">
-            <h1 class=" text-xs font-medium text-gray-400">NIC</h1>
-            <h1 class="text-base font-medium text-gray-700" v-if="userState.user.nic">{{ userState.user.nic }} </h1>
-            <h1 class="text-base font-medium text-gray-700" v-else>-</h1>
-          </div>
-
-          <div class=" text-left">
-            <h1 class=" text-xs font-medium text-gray-400">Report Person</h1>
-            <h1 class="text-base font-medium text-gray-700" v-if="userState.user.reportPerson.name">{{
-              userState.user.reportPerson.name }} </h1>
-            <h1 class="text-base font-medium text-gray-700" v-else>-</h1>
-          </div>
-
-          <div class=" text-left">
-            <h1 class=" text-xs font-medium text-gray-400">Adderss</h1>
-            <h1 class="text-base font-medium text-gray-700" v-if="userState.user.address">{{ userState.user.address }}
-            </h1>
-            <h1 class="text-base font-medium text-gray-700" v-else>-</h1>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <user-details-component :user="userState.user"/>
 
   <!-- Edit btn -->
   <button class=" bg-slate-600 rounded-full text-white fixed right-4 bottom-4 w-12 h-12" @click="openPopup()">
@@ -144,7 +85,9 @@
 import { mapGetters } from 'vuex'
 import moment from 'moment'
 import { NotificationHelper } from "@/helper/NotificationHelper";
+import UserDetailsComponent from '@/components/UserDetailsComponent.vue';
 export default {
+  components: { UserDetailsComponent },
   data() {
     return {
       page: 1,
